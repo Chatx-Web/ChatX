1ce9debe6c39baaf4819934f14f88643bd475c9c
hit-bheda <hitbheda.h@gmail.com>
2024-08-30 22:18:20 +0530

    fix: Shifted The App.tsx To pages/Home/Home.tsx

--- client/src/app-router.tsx
+++ client/src/app-router.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App";
+import Home from "./pages/Home";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" Component={App} />
+        <Route path="/" Component={Home} />
       </Routes>
     </BrowserRouter>
   );
