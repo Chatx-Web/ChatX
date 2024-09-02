9b2a454c38b2664ca4260432d9f1ccb0c1ae6808
hit-bheda <hitbheda.h@gmail.com>
2024-09-02 21:44:10 +0530

    feat: Created Error Handler Middleware And TryCatch Utility Function

---
+++ server/src/middlewares/error-handler.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { ErrorResponse } from "../api/v1/types/types";
+
+type ErrorRequestHandler = (
+    err: Error,
+    req: Request,
+    res: Response<ErrorResponse>,
+    next: NextFunction
+) => void;
+
+export const errorHandler: ErrorRequestHandler = ( err, req, res, next ) => {
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  });
+}
