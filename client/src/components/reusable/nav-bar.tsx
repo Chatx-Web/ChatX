a72ad960a08fb2b36e9e87c400286188f0607c6c
aissa-dev1 <aissabedr74@gmail.com>
2024-09-11 16:45:58 +0100

    feat: make the navbar full responsive in both large and small devices and map links data

--- client/src/components/reusable/nav-bar.tsx
+++ client/src/components/reusable/nav-bar.tsx
@@ -2,13 +2,19 @@ import Container from "./container";
 import Logo from "./logo";
 import Button from "./button";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { createStyle } from "../../utils/create-style";
+import { navLinks } from "../../data";
 
-interface NavLinkProps {
+export type NavLinkType = {
+  id: string;
   to: string;
   content: string;
-}
+};
 
-function NavLink({ to, content }: NavLinkProps) {
+interface NavLinkProps extends Omit<NavLinkType, "id"> {}
+
+function LargeNavLink({ to, content }: NavLinkProps) {
   return (
     <Link
       to={to}
@@ -19,28 +25,105 @@ function NavLink({ to, content }: NavLinkProps) {
   );
 }
 
+function LargeScreenNav() {
+  return (
+    <Container className="items-center justify-between hidden md:flex">
+      <Logo />
+      <div className="space-x-4">
+        {navLinks.map((navLink) => (
+          <LargeNavLink key={navLink.id} {...navLink} />
+        ))}
+      </div>
+      <div className="space-x-4">
+        <Link to="/signup">
+          <Button className="px-2 py-1" variant="primary">
+            Sign up
+          </Button>
+        </Link>
+        <Link to="/login">
+          <Button className="px-2 py-1">Login</Button>
+        </Link>
+      </div>
+    </Container>
+  );
+}
+
+function SmallNavLink({ to, content }: NavLinkProps) {
+  return (
+    <Link to={to}>
+      <div className="px-2 py-1 rounded-md border border-[var(--border)] hover:border-[var(--primary)] duration-300">
+        <p>{content}</p>
+      </div>
+    </Link>
+  );
+}
+
+function SmallScreenNav() {
+  const [menuActive, setMenuActive] = useState(false);
+
+  useEffect(() => {
+    if (menuActive) {
+      createStyle(document.body, {
+        overflow: "hidden",
+      });
+    } else {
+      createStyle(document.body, {
+        overflow: "auto",
+      });
+    }
+  }, [menuActive]);
+
+  return (
+    <Container className="flex items-center justify-between md:hidden">
+      <Logo />
+      <Button className="px-2 py-1" onClick={() => setMenuActive(true)}>
+        Menu
+      </Button>
+      {menuActive && (
+        <div
+          className="fixed top-0 left-0 w-screen h-screen bg-[var(--bg-color)]"
+          onClick={() => setMenuActive(false)}
+        >
+          <div
+            className="absolute left-0 top-1/2 -translate-y-1/2 bg-[var(--bg-color)] w-full p-8"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="flex items-center justify-between">
+              <p>Hello nav!</p>
+              <Button
+                className="px-2 py-1"
+                onClick={() => setMenuActive(false)}
+              >
+                Close
+              </Button>
+            </div>
+            <div className="flex flex-col gap-3 mt-4">
+              {navLinks.map((navLink) => (
+                <SmallNavLink key={navLink.id} {...navLink} />
+              ))}
+              <div className="grid grid-cols-2 gap-4">
+                <Link to="/signup">
+                  <Button className="w-full py-2" variant="primary">
+                    Sign up
+                  </Button>
+                </Link>
+                <Link to="/login">
+                  <Button className="w-full py-2">Login</Button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </Container>
+  );
+}
+
 export default function NavBar() {
   return (
     <nav className="fixed top-0 left-0 z-10 flex flex-col justify-center w-full h-20 bg-opacity-25 shadow-sm shadow-white/10 backdrop-blur-lg backdrop-filter">
-      <Container className="flex items-center justify-between">
-        <Logo />
-        <div className="space-x-4">
-          <NavLink to="/" content="Home" />
-          <NavLink to="/about" content="About" />
-          <NavLink to="/contact" content="Contact" />
-          <NavLink to="/services" content="Services" />
-        </div>
-        <div className="space-x-4">
-          <Link to="/signup">
-            <Button className="px-2 py-1" variant="primary">
-              Sign up
-            </Button>
-          </Link>
-          <Link to="/login">
-            <Button className="px-2 py-1">Login</Button>
-          </Link>
-        </div>
-      </Container>
+      <LargeScreenNav />
+      <SmallScreenNav />
     </nav>
   );
 }
